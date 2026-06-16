1aaefa71af784a0c069a1111536c0c950c73db2f
Kacy Do <contact@kacydo.dev>
2026-06-17 03:35:33 +0700

    modify hero section to display additional medias and upcoming event

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,14 +12,14 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || []
 
   return (
-    <nav className="flex gap-3 items-center">
+    <nav className='flex gap-3 items-center'>
       {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />
+        return <CMSLink key={i} {...link} appearance='link' />
       })}
-      <Link href="/search">
-        <span className="sr-only">Search</span>
-        <SearchIcon className="w-5 text-primary" />
-      </Link>
+      {/*<Link href="/search">*/}
+      {/*  <span className="sr-only">Search</span>*/}
+      {/*  <SearchIcon className="w-5 text-primary" />*/}
+      {/*</Link>*/}
     </nav>
   )
 }
