9e4415e47f0c384a4b34e8a4e0dec57336aebc74
Kacy Do <contact@kacydo.dev>
2026-06-26 14:28:42 +0700

    add Trusted Brand block render

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -7,6 +7,7 @@ import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { ContentBlock } from '@/blocks/Content/Component'
 import { FormBlock } from '@/blocks/Form/Component'
 import { MediaBlock } from '@/blocks/MediaBlock/Component'
+import { TrustedBrandsBlock } from '@/blocks/TrustedBrands'
 
 const blockComponents = {
   archive: ArchiveBlock,
@@ -14,6 +15,8 @@ const blockComponents = {
   cta: CallToActionBlock,
   formBlock: FormBlock,
   mediaBlock: MediaBlock,
+  // TODO: convert all block slug to kebab style
+  'trusted-brands': TrustedBrandsBlock,
 }
 
 export const RenderBlocks: React.FC<{
